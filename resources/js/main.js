6390e370e09f5990d85a71be1305a6f09ca1c9b3
ritsarassi <38128514+ritsarassi@users.noreply.github.com>
2018-05-25 17:58:31 +0300

    progress

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
