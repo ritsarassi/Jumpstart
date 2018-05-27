1ae4b6bc884eccf06b9b101e5ae8f1449f3e6605
ritsarassi <38128514+ritsarassi@users.noreply.github.com>
2018-05-27 16:34:46 +0300

    still need some fixing

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  });
+});
